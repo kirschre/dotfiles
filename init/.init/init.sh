85cba3084b8262c716b3541b6ec75a5828d35994
Rob Kirschner <rkirschner@factset.com>
2018-09-10 15:13:25 -0400

    feat(init): add some things to be loaded by bashrc

---
+++ init/.init/init.sh
@@ -0,0 +1,13 @@
+# https://github.com/creationix/nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+export EDITOR=vim
+export VISUAL=$EDITOR
+export PAGER=less
+export LESS=-mMRXF
+
+source ./git-prompt.sh
+
+PS1='\[\e[00;32m\]┌[\T \u@\h]\[\e[0m\]\[\e[00;35m\]$(__git_ps1 "(%s)")\[\e[0m\]\[\e[00;32m\]\n[\w]\[\e[0m\] '
