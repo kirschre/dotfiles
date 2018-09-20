9d28e174d2ba4dfe4c0e246e2718cee6c7c952e3
Rob Kirschner <rkirschner@factset.com>
2018-09-20 15:17:30 -0400

    fix(tmux): alias tmux to automatically pull in config

--- init/.init/init.sh
+++ init/.init/init.sh
@@ -3,6 +3,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+alias tmux="tmux -2 -u -f ~/.tmux/tmux.conf"
+
 export PATH=$PATH:$(yarn global bin)
 
 export EDITOR=vim
