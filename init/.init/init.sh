24297f63850de2d6097464fe7cbf107427ff35cf
Rob Kirschner <rkirschner@factset.com>
2018-09-20 15:14:15 -0400

    feat(yarn): add yarn global path

--- init/.init/init.sh
+++ init/.init/init.sh
@@ -3,11 +3,11 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+export PATH=$PATH:$(yarn global bin)
+
 export EDITOR=vim
 export VISUAL=$EDITOR
 export PAGER=less
 export LESS=-mMRXF
-
-source ./git-prompt.sh
 
 PS1='\[\e[00;32m\]┌[\T \u@\h]\[\e[0m\]\[\e[00;35m\]$(__git_ps1 "(%s)")\[\e[0m\]\[\e[00;32m\]\n[\w]\[\e[0m\] '
